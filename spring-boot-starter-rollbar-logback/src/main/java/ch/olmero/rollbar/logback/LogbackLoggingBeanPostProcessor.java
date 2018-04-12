10a5e884f5345dbe7c5bf5da506611d0e89b1afe
Philip Kaufmann <philip.kaufmann@olmero.ch>
2018-04-12 13:57:51 +0200

    fix initialization of logback appender

---
+++ spring-boot-starter-rollbar-logback/src/main/java/ch/olmero/rollbar/logback/LogbackLoggingBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package ch.olmero.rollbar.logback;
+
+import ch.olmero.rollbar.RollbarNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@RequiredArgsConstructor
+public class LogbackLoggingBeanPostProcessor implements BeanPostProcessor {
+
+	private final LogbackLoggingConfigurer logbackLoggingConfigurer;
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof RollbarNotificationService) {
+			logbackLoggingConfigurer.configure((RollbarNotificationService)bean);
+		}
+
+		return bean;
+	}
+}
